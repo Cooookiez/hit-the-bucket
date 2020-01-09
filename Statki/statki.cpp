cf2ec52724e40a868766505a58c4a812142ae951
Krzysiek (Chris) <kukizk@gmail.com>
2020-01-09 09:38:50 +0100

    dzwięk bierze z folderu relatywnego?

--- Statki/statki.cpp
+++ Statki/statki.cpp
@@ -4,6 +4,7 @@
 #include <QSound>
 #include <QtMultimedia/QMediaPlayer>
 #include <iostream>
+#include <QDir>
 
 Statki::Statki(QWidget *parent): QMainWindow(parent), ui(new Ui::Statki){
 
@@ -35,19 +36,21 @@ void Statki::play_sound()
 {
     //QSound dzwiek("Ring06.wav");
     //dzwiek.play();
-    QSound::play("../sound/Ring06.wav");
-    qDebug() << "App path : " << qApp->applicationDirPath();
+//    QSound::play(QUrl::fromLocalFile("../Ring06.wav"));
+//    qDebug() << "App path : " << qApp->applicationDirPath();
 //    printf(qApp->applicationDirPath())
 //    std::cout << "qApp->applicationDirPath();" << qApp->applicationDirPath() << std::endl;
 }
 void Statki::play_sound_1()
 {
     QMediaPlayer *player = new QMediaPlayer;
-    player->setMedia(QUrl::fromLocalFile("../sound/Ring06.wav"));
+    player->setMedia(QUrl::fromLocalFile(QDir::cleanPath(qApp->applicationDirPath() + QDir::separator() + "../../../../sounds/Ring06.wav")));
     player->setVolume(50);
     player->play();
     qDebug() << "App path : " << qApp->applicationDirPath();
+    qDebug() << "Snd path : " << QUrl::fromLocalFile(QDir::cleanPath(qApp->applicationDirPath() + QDir::separator() + "../../../../sounds/Ring06.wav")).toString();
 }
+
 
 void Statki::on_btn_Connect_clicked()
 {
